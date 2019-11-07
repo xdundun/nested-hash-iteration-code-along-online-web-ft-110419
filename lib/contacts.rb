65cfec6632b1c85a34efd9aa1d22c1b88b48710c
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 21:55:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,8 +20,8 @@ def remove_strawberry(contacts)
         contact_details_hash.each do |attribute, data|
           if attribute == :favorite_ice_cream_flavors
             data.delete_if {|ice_cream| ice_cream == "strawberry"}
-          end
         end
       end
     end
   end
+end
